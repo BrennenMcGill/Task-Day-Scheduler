3f8a593662fd8e8b9b0532df84a35eceda0b902b
BrennenMcGill <31702142+BrennenMcGill@users.noreply.github.com>
2020-05-30 00:29:07 -0600

    finished html

---
+++ Develop/js/script.js
@@ -0,0 +1,23 @@
+var ninethHour = 9;
+var tenthHour = 10;
+var eleventhHour = 11;
+var twelthHour = 12;
+var thirteenthHour = 13;
+var fourteenthHour = 14;
+var fifteenthHour = 15;
+var sixteenthHour = 16;
+var seventeenthHour = 17;
+var currentHour = moment().format("H");
+
+$("#currentDay").text(moment().format("MMM DD, YYYY"));
+
+
+
+
+
+
+
+
+
+
+
